f492fe65a8636059918d126833f928b0d19006e5
AlekefromKz <kydyrmin.almaz@gmail.com>
2021-02-28 11:33:09 +0600

    Configured Postgres

--- biometric/settings.py
+++ biometric/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'biometric.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'biometric',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
